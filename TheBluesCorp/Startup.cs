2f47a4c6a6f23bf0ffa6354689cfda23edb85733
TREVOR WILLIAMS <trevorcwilliams@gmail.com>
2015-02-03 04:17:00 -0800

    first commit

---
+++ TheBluesCorp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheBluesCorp.Startup))]
+namespace TheBluesCorp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
